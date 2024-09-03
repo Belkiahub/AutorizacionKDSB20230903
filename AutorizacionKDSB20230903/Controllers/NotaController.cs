e7be5d948559ecf12dad8c2ded3df881f8ea4795
KevinSilva <kevinsilva462@gmail.com>
2024-09-03 14:07:22 -0600

    Nota controlador

---
+++ AutorizacionKDSB20230903/Controllers/NotaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AutorizacionKDSB20230903.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotaController : ControllerBase
+    {
+        static List<object> data = new List<object>();
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IEnumerable<object> Get()
+        {
+            return data;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Post(string name, string Note)
+        {
+            data.Add(new { name, Note });
+            return Ok();
+        }
+    }
+}
